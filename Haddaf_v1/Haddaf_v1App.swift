e8d2c9558b9314e47ce6a98824546b7d7b1d8a6e
Leen Thamer <89545353+Leen0th@users.noreply.github.com>
2025-10-06 23:27:13 +0300

    Initial Commit

---
+++ Haddaf_v1/Haddaf_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Haddaf_v1App.swift
+//  Haddaf_v1
+//
+//  Created by Leen Thamer on 06/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Haddaf_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Haddaf_v1Tests/Haddaf_v1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Haddaf_v1Tests.swift
+//  Haddaf_v1Tests
+//
+//  Created by Leen Thamer on 06/10/2025.
+//
+
+import Testing
+
+struct Haddaf_v1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
