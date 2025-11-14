6498b9da5d6c1f426ea72d86b72b185781c6a870
alhussan <lal7ussan@gmail.com>
2025-11-14 14:42:13 +0300

    add comments

--- Haddaf_v1/SwiftUI screens/ReportedContentView.swift
+++ Haddaf_v1/SwiftUI screens/ReportedContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ReportedContentView.swift
-//  Haddaf_v1
-//
-//  Created by Leen Thamer on 07/11/2025.
-//
-
 import SwiftUI
 
 /// A placeholder view that hides content the user has reported.
@@ -18,25 +11,24 @@ struct ReportedContentView: View {
     }
 
     var body: some View {
-        VStack(alignment: .center, spacing: isComment ? 8 : 16) { // Smaller spacing for comment
+        VStack(alignment: .center, spacing: isComment ? 8 : 16) {
             Image(systemName: "flag.fill")
-                .font(isComment ? .body : .title) // Smaller icon for comment
+                .font(isComment ? .body : .title)
                 .foregroundColor(.secondary)
             
             Text("You reported this \(type.rawValue.lowercased()).")
-                .font(.system(size: isComment ? 14 : 16, weight: .medium, design: .rounded)) // Smaller text for comment
-                .foregroundColor(.secondary)
+                .font(.system(size: isComment ? 14 : 16, weight: .medium, design: .rounded))                .foregroundColor(.secondary)
             
             Button("View Content") {
                 onShow()
             }
-            .font(.system(size: isComment ? 13 : 14, weight: .bold, design: .rounded)) // Smaller button for comment
+            .font(.system(size: isComment ? 13 : 14, weight: .bold, design: .rounded))
             .foregroundColor(BrandColors.darkTeal)
         }
         .frame(maxWidth: .infinity)
         .padding(.vertical, isComment ? 10 : 20) 
         .padding(.horizontal)
         .background(BrandColors.lightGray.opacity(0.7))
-        .cornerRadius(isComment ? 12 : 20) // Smaller corner radius for comment
+        .cornerRadius(isComment ? 12 : 20)
     }
 }
